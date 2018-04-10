e8893696aba85b4a4fc692095f20cb49d766c5eb
rajat bhardwaj <rajatb94@gmail.com>
2018-04-10 17:19:51 +0530

    new clone first branch commit

--- f2.js
+++ f2.js
@@ -7,8 +7,10 @@ for(var i=0;i<3;i++){
 function findNow(){
     var a=4;
     a=a*3+4-3/2;
+	var b="sjgjhgjhs";
     if(name.includes("a"))
         console.log(name);
+	var a="ddd";
 }
 
 findNow();
