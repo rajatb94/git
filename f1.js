cda7cdede550c8aa109756cf42fb66ddb9d1e318
rajat bhardwaj <rajatb94@gmail.com>
2018-04-10 16:23:15 +0530

    first

---
+++ f1.js
@@ -0,0 +1,12 @@
+var a=1;
+
+var b=a+453;
+
+var c=[a,b];
+
+function kuch(){
+    c.forEach((el)=>{
+        console.log(c);
+    })
+}
+kuch();
---
+++ f2.js
@@ -0,0 +1,12 @@
+var name="Rajat Bhardwaj"
+
+for(var i=0;i<3;i++){
+    name+=name;
+}
+
+function findNow(){
+    if(name.includes("a"))
+        console.log(name);
+}
+
+findNow();
